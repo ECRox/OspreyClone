2382265a2349065d3ce55c51d8ae82a48aa686d8
howdycrystalm <howdycrystalm@gmail.com>
2017-02-18 21:02:52 -0800

    finished mobile black header placement

--- public/js/main/bannerDirective.js
+++ public/js/main/bannerDirective.js
@@ -1,8 +1,14 @@
 angular.module('ospreypack').directive('bannerDirective', function() {
 
+  // return {
+  //   restruct: 'E',
+  //   templateUrl: './views/bannerTmpl.html',
+  //   controller: 'bannerCtrl'
+  // }
+
   return {
     restruct: 'E',
-    templateUrl: './views/bannerTmpl.html',
+    templateUrl: './views/bannerTmpl2.html',
     controller: 'bannerCtrl'
   }
 
