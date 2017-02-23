55dac2909ba70c3c2dd59daab26c92276bf43479
howdycrystalm <howdycrystalm@gmail.com>
2017-02-23 14:20:07 -0800

    modal popping up

--- public/js/main/bannerCtrl.js
+++ public/js/main/bannerCtrl.js
@@ -4,34 +4,50 @@ angular.module("ospreypack")
 .controller("bannerCtrl", function($scope) {
   // VARIABLES
   // ============================================================
-  var mega = document.getElementsByClassName("mega");
-  var i;
+  $scope.showModal = false;
 
-  // FUNCTIONS
-  // ============================================================
-  for (i = 0; i < mega.length; i++) {
-    mega[i].onclick = function() {
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if (panel.style.maxHeight){
-          panel.style.maxHeight = null;
-      } else {
-          panel.style.maxHeight = panel.scrollHeight + 'px';
-      }
-    }
-  }
 
-//   $(function(){
-//   $('.animated > li').hover(function(){
-//     $(this).find('div[class^="container-"]').stop().slideDown('fast');
-//   },
-//   function(){
-//     $(this).find('div[class^="container-"]').stop().slideUp('slow');
-//   });
-// });
-  // $(document).ready(function() {
-  //   $('.megaordion-head').click(function() {
-  //     $('.menu-content').toggleClass('visible');
-  //   });
-  // });
+  // var mega = document.getElementsByClassName("mega");
+  // var i;
+
+//   // FUNCTIONS
+//   // ============================================================
+//   for (i = 0; i < mega.length; i++) {
+//     mega[i].onclick = function() {
+//       this.classList.toggle("active");
+//       var panel = this.nextElementSibling;
+//       if (panel.style.maxHeight){
+//           panel.style.maxHeight = null;
+//       } else {
+//           panel.style.maxHeight = panel.scrollHeight + 'px';
+//       }
+//     }
+//   }
+// //********************************** MODAL **********************************//
+//
+//   // Get the modal
+//   var modal = document.getElementById('myModal');
+//
+//   // Get the button that opens the modal
+//   var btn = document.getElementById("myBtn");
+//
+//   // Get the <span> element that closes the modal
+//   var span = document.getElementsByClassName("close")[0];
+//
+//   // When the user clicks on the button, open the modal
+//   btn.onclick = function() {
+//       modal.style.display = "block";
+//   }
+//
+//   // When the user clicks on <span> (x), close the modal
+//   span.onclick = function() {
+//       modal.style.display = "none";
+//   }
+//
+//   // When the user clicks anywhere outside of the modal, close it
+//   window.onclick = function(event) {
+//       if (event.target == modal) {
+//           modal.style.display = "none";
+//       }
+//   }
 });
