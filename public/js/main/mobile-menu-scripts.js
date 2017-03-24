e4abc62617b6b92a773b4cb20ba11ed4f0b4548d
howdycrystalm <howdycrystalm@gmail.com>
2017-03-24 15:22:36 -0700

    added flag to drop down

--- public/js/main/mobile-menu-scripts.js
+++ public/js/main/mobile-menu-scripts.js
@@ -1,6 +1,6 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidenav").style.width = "70%";
+    document.getElementById("main").style.marginLeft = "70%";
   //   $('.white-nav').find('.icon')
   // .toggleClass('<i class="material-icons md-48">menu</i> <i class="material-icons md-48">close</i>');
 }
